92f967e66c72e49eddd40279d12181f30818b59b
@ochumelyi_maks <only-max@yandex.ru>
2025-07-01 09:25:00 +0300

    create ini

--- lesson4/calculator_test.py
+++ lesson4/calculator_test.py
@@ -2,6 +2,17 @@ from calculator import Calculator
 
 calculator = Calculator()
 
+def test_sum_positive_nums():
+  calculator = Calculator()
+  res = calculator.sum(4, 5)
+  assert res == 9
+
+
+def test_sum_negative_nums():
+  calculator = Calculator()
+  res = calculator.sum(-6, -10)
+  assert res == -16
+
 # + +
 # - - 
 # - +
