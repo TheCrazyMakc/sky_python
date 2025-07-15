a9da8d14bfc30ac70f314e47843daa258e1b1ce7
@ochumelyi_maks <only-max@yandex.ru>
2025-07-15 14:24:32 +0300

    ready task 4

--- 05_lesson/lesson05_task4.py
+++ 05_lesson/lesson05_task4.py
@@ -9,17 +9,25 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 try: 
-  driver.get("http://the-internet.herokuapp.com/inputs")
+  driver.get("http://the-internet.herokuapp.com/login")
 
-  search_input = driver.find_element(By.CSS_SELECTOR, 'input[type="number"]')
+  search_input_login = driver.find_element(By.CSS_SELECTOR, '#username')
 
-  search_input.send_keys("Sky")
+  search_input_login.send_keys("tomsmith")
 
-  search_input.clear()
+  search_input_password = driver.find_element(By.CSS_SELECTOR, '#password')
 
-  search_input.send_keys("Pro")
+  search_input_password.send_keys("SuperSecretPassword!")
 
-  sleep(15)
+  search_button_login = driver.find_element(By.CSS_SELECTOR, 'button.radius')
+
+  search_button_login.click()
+
+  div_text = driver.find_element(By.CSS_SELECTOR, 'div.flash.success').text
+
+  print("Текст из div:", div_text)
+
+  sleep(10)
 
 finally:
   driver.quit()
