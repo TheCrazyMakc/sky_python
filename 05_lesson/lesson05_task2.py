50f61d0b3fb495e2fd150beea6c3876c6d9953db
@ochumelyi_maks <only-max@yandex.ru>
2025-07-15 13:10:54 +0300

    create second file and ready task 2

---
+++ 05_lesson/lesson05_task2.py
@@ -0,0 +1,24 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+try:
+  driver.get("https://www.google.com")
+
+  driver.get("http://uitestingplayground.com/dynamicid")
+
+  search_btn = driver.find_element(By.CSS_SELECTOR, "button.btn-primary")
+
+  search_btn.click()
+
+  sleep(10)
+
+finally:
+  driver.quit()
