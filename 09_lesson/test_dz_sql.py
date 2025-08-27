d079fb7b87dc7b5e36eb6ad3168482088e41fc12
@ochumelyi_maks <only-max@yandex.ru>
2025-08-27 11:11:04 +0300

    pep8 ready

--- 09_lesson/test_dz_sql.py
+++ 09_lesson/test_dz_sql.py
@@ -5,6 +5,7 @@ from sqlalchemy.sql import text
 db_connection_string = "postgresql://qa:skyqa@5.101.50.27:5432/x_clients"
 db = create_engine(db_connection_string)
 
+
 # находим список всех таблиц
 def test_get_info():
     # Используем инспектор для получения информации о таблицах
@@ -12,7 +13,9 @@ def test_get_info():
     names = inspector.get_table_names()
     print("Все таблицы в базе:", names)
 
+
 test_get_info()
+
 
 # находим таблицу company
 def test_get_table_company():
@@ -20,8 +23,11 @@ def test_get_table_company():
     rows = db.execute("select * from company").fetchall()
     print("Таблица company:", rows)
 
+
 test_get_table_company()
 
+
+# добавляем данные в таблицу
 def test_add_company():
     # SQL запрос для вставки данных
     sql = text("INSERT INTO company (id, is_active, name, description) VALUES (:id, :is_active, :name, :description)")
@@ -36,13 +42,16 @@ def test_add_company():
 
     # Выполняем запрос
     with db.connect() as connection:
-        result = connection.execute(sql, company_data)
+        connection.execute(sql, company_data)
         connection.commit()
 
     print("Компания успешно добавлена!")
 
+
 test_add_company()
 
+
+# редактируем данные в таблице
 def test_update_company():
     # SQL запрос для обновления данных
     sql = text("UPDATE company SET name = :new_name, description = :new_description WHERE id = :id")
@@ -56,13 +65,16 @@ def test_update_company():
 
     # Выполняем запрос
     with db.connect() as connection:
-        result = connection.execute(sql, update_data)
+        connection.execute(sql, update_data)
         connection.commit()
 
     print("Данные компании успешно обновлены!")
 
+
 test_update_company()
 
+
+# удаляем данные из таблицы
 def test_delete_company():
     # SQL запрос для удаления
     sql = text("DELETE FROM company WHERE id = :id")
@@ -74,9 +86,10 @@ def test_delete_company():
 
     # Выполняем запрос
     with db.connect() as connection:
-        result = connection.execute(sql, delete_data)
+        connection.execute(sql, delete_data)
         connection.commit()
 
     print("Компания успешно удалена!")
+
 
 test_delete_company()
