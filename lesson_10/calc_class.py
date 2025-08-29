25f1c07625dac37a8e38a9c6509cb380424688c8
@ochumelyi_maks <only-max@yandex.ru>
2025-08-29 14:50:13 +0300

    add allure and comments to def in calc_class.py

--- lesson_10/calc_class.py
+++ lesson_10/calc_class.py
@@ -17,11 +17,21 @@ class CalculatorPage:
             "slow-calculator.html"
         )
 
+    @allure.step("Открытие страницы калькулятора")
     def open(self):
+        """
+        Открывает страницу калькулятора.
+        """
         self.driver.get(self.url)
         return self
 
+    @allure.step("Установка задержки {seconds} секунд")
     def set_delay(self, seconds):
+        """
+        Устанавливает задержку для выполнения операций на калькуляторе.
+
+        :param seconds: int — время задержки в секундах.
+        """
         delay_input = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "#delay")
             )
@@ -30,7 +40,13 @@ class CalculatorPage:
         delay_input.send_keys(str(seconds))
         return self
 
+    @allure.step("Нажатие кнопки '{button_text}'")
     def click_button(self, button_text):
+        """
+        Нажимает на кнопки калькулятора.
+
+        :param button_text: str — текст на кнопке, которую нужно нажать.
+        """
         button = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(
                 (By.XPATH, f"//span[text()='{button_text}']")
@@ -39,7 +55,11 @@ class CalculatorPage:
         button.click()
         return self
 
+    @allure.step("Получение результата с экрана калькулятора")
     def get_result(self, timeout=50):
+        """
+        Возвращает текущий результат с экрана калькулятора.
+        """
         WebDriverWait(self.driver, timeout).until(
             EC.text_to_be_present_in_element(
                 (By.CSS_SELECTOR, ".screen"), "15"
