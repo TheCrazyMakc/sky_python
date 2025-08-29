6b18eec6658d1188aa50e58a38cb47be39d3e6af
@ochumelyi_maks <only-max@yandex.ru>
2025-08-29 14:22:52 +0300

    start homework - add old files to project

---
+++ lesson_10/calc_class.py
@@ -0,0 +1,42 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class CalculatorPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.url = (
+            "https://bonigarcia.dev/selenium-webdriver-java/"
+            "slow-calculator.html"
+        )
+
+    def open(self):
+        self.driver.get(self.url)
+        return self
+
+    def set_delay(self, seconds):
+        delay_input = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#delay")
+            )
+        )
+        delay_input.clear()
+        delay_input.send_keys(str(seconds))
+        return self
+
+    def click_button(self, button_text):
+        button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, f"//span[text()='{button_text}']")
+            )
+        )
+        button.click()
+        return self
+
+    def get_result(self, timeout=50):
+        WebDriverWait(self.driver, timeout).until(
+            EC.text_to_be_present_in_element(
+                (By.CSS_SELECTOR, ".screen"), "15"
+            )
+        )
+        return self.driver.find_element(By.CSS_SELECTOR, ".screen").text
